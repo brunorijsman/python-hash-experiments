d15209219f2def25815a6a84ddbfc81a019e7f30
Bruno Rijsman <brunorijsman@gmail.com>
2019-05-01 10:17:02 -0400

    Update to use HMAC

--- experiment.py
+++ experiment.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 import hashlib
+import hmac
 import timeit
 
-NUMBER_HASHES = 10000
+NUMBER_DIGESTS = 100000
 
+SECRET = "This-is-my-super-duper-secret-key-12345#"
 
-def hash_loop(hash_algo, data):
-    _digest = hash_algo(data).digest()
+def digest_loop(digestmod, data):
+    the_hmac = hmac.new(SECRET.encode(), digestmod=digestmod)
+    the_hmac.update(data)
+    _digest = the_hmac.digest()
 
-def hash_measurement(hash_algo, hash_algo_name, data_len):
+def digest_measurement(digestmod, digestmod_name, data_len):
     data = b'\xab' * data_len
-    measured_function = lambda: hash_loop(hash_algo, data)
-    time_in_secs = timeit.timeit(measured_function, number=NUMBER_HASHES)
-    hashes_per_second = NUMBER_HASHES / time_in_secs
-    print("{:10}  {:10,}  {:10,}".format(hash_algo_name, data_len, int(hashes_per_second)))
+    measured_function = lambda: digest_loop(digestmod, data)
+    time_in_secs = timeit.timeit(measured_function, number=NUMBER_DIGESTS)
+    hashes_per_second = NUMBER_DIGESTS / time_in_secs
+    usecs_per_hash = 1000000 / hashes_per_second
+    print("{:12}  {:12,}  {:12,}  {:12,}".format(digestmod_name, data_len, int(hashes_per_second),
+                                         int(usecs_per_hash)))
 
 def experiment():
-    print("{:10}  {:10}  {:10}".format("Algo", "Data size", "Hashes/sec"))
-    line = '-' * 10
-    print("{:10}  {:10}  {:10}".format(line, line, line))
-    for data_len in [10, 100, 1000, 10000]:
-        for hash_algo, hash_algo_name in [(hashlib.md5, "MD-5"),
+    print("{:12}  {:12}  {:12}  {:12}".format("Algo", "Data size", "Digests/sec", "Usec/digest"))
+    line = '-' * 12
+    print("{:12}  {:12}  {:12}  {:12}".format(line, line, line, line))
+    for data_len in [10, 100, 1500, 10000]:
+        for digestmod, digestmod_name in [(hashlib.md5, "MD-5"),
                                           (hashlib.sha1, "SHA-1"),
                                           (hashlib.sha224, "SHA-224"),
                                           (hashlib.sha256, "SHA-256"),
@@ -33,7 +39,7 @@ def experiment():
                                           (hashlib.sha3_512, "SHA3-512"),
                                           (hashlib.blake2b, "BLAKE-2B"),
                                           (hashlib.blake2s, "BLAKE-2S")]:
-            hash_measurement(hash_algo, hash_algo_name, data_len)
+            digest_measurement(digestmod, digestmod_name, data_len)
 
 if __name__ == "__main__":
     experiment()
